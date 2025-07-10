a33a6756860f4a4759879b41cef7303199eb0611
denni5x <denrihm@gmail.com>
2025-07-11 00:28:13 +0200

    changed version to 1.21.4 for debugging

--- src/client/java/com/denni5x/cobblet/client/CobbletClient.java
+++ src/client/java/com/denni5x/cobblet/client/CobbletClient.java
@@ -4,9 +4,12 @@ import com.moulberry.axiom.tools.ToolManager;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.moulberry.axiom.utils.Authorization;
 
 public class CobbletClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("cobblet");
+    
+
 
     @Override
     public void onInitializeClient() {
